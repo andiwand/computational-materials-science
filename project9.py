63b168a80321223b5ae8875e1de706890db94a16
Andreas Stefl <stefl.andreas@gmail.com>
2019-03-21 01:27:08 +0100

    improve project9; add ue3.pdf; improve ue3

--- project9.py
+++ project9.py
@@ -3,42 +3,30 @@
 import numpy as np
 from numpy.linalg import multi_dot
 import matplotlib.pyplot as plt
-import pandas as pd
 from scipy.misc import comb
 from scipy import sparse
 
-# from https://github.com/mwallerb/LAZY/blob/master/ED.ipynb
-def get_annihilators(flavors, dtype=np.float64):
+def get_annihilators(flavors, format=None, dtype=np.int8):
     dim_fock = 2**flavors
-    fock_range = np.arange(dim_fock)
-
     c = [None] * flavors
-    sign = np.ones(dim_fock, np.int8)
-    bitmask = 1
     for i in range(flavors):
-        # The operator c_i removes the state i, which is equivalent to
-        # shifting the Fock state 2**i to the left or occupying the +2**i'th
-        # side diagonal, but only if the state is indeed present (has_i).
-        # Diagonal storage always starts at column 0 (the first rows in the
-        # c operators being "above" the matrix)
-        has_i = np.array(fock_range & bitmask, dtype=np.bool)
-        c[i] = sparse.dia_matrix((has_i * sign, bitmask),
-                                 shape=(dim_fock, dim_fock),
-                                 dtype=dtype).todense()
-
-        # The sign is (-1)**n, where n is the number of occupied states
-        # smaller than (ordered before) i.  So, for the next step we flip
-        # the sign of those elements where the current state is present.
-        sign[has_i] *= -1
-        bitmask <<= 1
+        eye = sparse.identity(2**i, format=format, dtype=dtype)
+        zero = sparse.csr_matrix(eye.shape, dtype=dtype)
+        for j in range(i):
+            c[j] = sparse.bmat([[c[j], zero], [zero, -c[j]]], format=format, dtype=dtype)
+        c[i] = sparse.bmat([[zero, eye], [zero, zero]], format=format, dtype=dtype)
     return c
 
 def get_creators(annihilators):
     return [a.H for a in annihilators]
 
 def get_ns(annihilators):
-    it = zip(annihilators[::2], annihilators[1::2])
-    return [au.H.dot(au) + ad.H.dot(ad) for au, ad in it]
+    return [a.H.dot(a) for a in annihilators]
+
+def get_nss(annihilators):
+    ns = get_ns(annihilators)
+    it = zip(ns[::2], ns[1::2])
+    return [nu + nd for nu, nd in it]
 
 def get_szs(annihilators):
     it = zip(annihilators[::2], annihilators[1::2])
@@ -52,17 +40,16 @@ def get_sz(annihilators):
 
 def get_s(annihilators):
     szs = get_szs(annihilators)
-    it = zip(szs, szs[1:] + szs[:1])
-    return 0.25 * sum(si.dot(sj) for si, sj in it)
+    return 0.25 * sum(si.dot(sj) for si, sj in zip(szs, szs[1:] + szs[:1]))
 
-def hamilton_ring(n, t, U, annihilators):
+def hamilton_ring(t, U, annihilators):
     c = annihilators
-    h = c[0].dot(c[1]) + c[1].dot(c[0])
-    sites = list(range(n))
+    h = sparse.csr_matrix(c[0].shape, dtype=c[0].dtype)
+    sites = list(range(len(annihilators) // 2))
     
     for i, j in zip(sites, sites[1:] + sites[:1]):
         for s in range(2):
-            h += -t * (c[2*i+s].H.dot(c[2*j+s]) + c[2*j+s].H.dot(c[2*i+s]))
+            h += t * (c[2*i+s].H.dot(c[2*j+s]) + c[2*j+s].H.dot(c[2*i+s]))
     
     for i in sites:
         h += U * (c[2*i+0].H.dot(c[2*i+1].H) * c[2*i+1].dot(c[2*i+0]))
@@ -84,83 +71,132 @@ def sz_sector_mask(dim_fock, sz):
     return mask
 
 def get_sector(operator, mask):
-    return operator[mask,:][:,mask]
+    result = operator[mask,:][:,mask]
+    if sparse.issparse(result):
+        result = result.toarray()
+    return result
 
-flavors = 8 # number of flavors
-dim_fock = 2**flavors
+def check_annihilators(annihilators):
+    flavors = len(annihilators)
+    dim_fock = 2**flavors
+
+    g = ((i, j) for i in range(flavors) for j in range(i, flavors))
+    for i, j in g:
+        ci, cj = annihilators[i], annihilators[j]
+
+        if ci.shape != (dim_fock, dim_fock):
+            print('ERROR ci has invalid shape for i=%i' % i)
+        if cj.shape != (dim_fock, dim_fock):
+            print('ERROR cj has invalid shape for j=%i' % i)
+
+        r = ci.dot(cj) + cj.dot(ci)
+        if len(sparse.find(r)[2]) > 0:
+            print('ERROR ci*cj+cj*ci != 0 for i=%i, j=%i' % (i, j))
+
+        r = ci.dot(cj.H) + cj.H.dot(ci)
+        if i != j and len(sparse.find(r)[2]) > 0:
+            print('ERROR ci*cj.H+cj.H*ci != 0 for i=%i, j=%i' % (i, j))
+        elif i == j and (r != np.eye(dim_fock)).any():
+            print('ERROR ci*ci.H+ci.H*ci != 1 for i=%i' % i)
+
+def check_H(annihilators):
+    H = hamilton_ring(1, 1, annihilators)
+    if not np.allclose(H.A, H.H.A):
+        print('ERROR H is not hermitian')
+    N = get_n(annihilators)
+    if not np.allclose(H.dot(N).A, N.dot(H).A):
+        print('ERROR H and N do not commute')
+    Sz = get_sz(annihilators)
+    if not np.allclose(H.dot(Sz).A, Sz.dot(H).A):
+        print('ERROR H and Sz do not commute')
+
+def part1(annihilators, mask, t, Us):
+    S = get_s(annihilators)
+    S = get_sector(S, mask)
+
+    Es = []
+    Sgs = []
+    for U in Us:
+        print('part1: U=%f' % U)
+        H = hamilton_ring(t, U, annihilators)
+        H = get_sector(H, mask)
+        w, v = np.linalg.eigh(H)
+
+        w[np.abs(w) < 1e-5] = 0
+        v[np.abs(v) < 1e-5] = 0
+        sorter = np.argsort(w)
+        w = w[sorter]
+        v = v[:,sorter]
+
+        s = multi_dot((v[:,0].T, S, v[:,0]))
+
+        Es.append(w)
+        Sgs.append(s)
+    Es = np.array(Es)
+    Sgs = np.array(Sgs)
+    
+    for i in range(Es.shape[1]):
+        plt.plot(Us, Es[:,i], label='E%d' % i)
+    plt.xlabel('U')
+    plt.ylabel('Ei')
+    #plt.legend()
+    plt.show()
+    
+    plt.plot(Us, Sgs)
+    plt.xlabel('U')
+    plt.ylabel('S(gs)')
+    plt.show()
+
+def part2(annihilators, mask, t, U, Ts):
+    Ss = []
+    
+    H = hamilton_ring(t, U, annihilators)
+    H = get_sector(H, mask)
+    w, v = np.linalg.eigh(H)
+    S = get_s(annihilators)
+    S = get_sector(S, mask)
+
+    sorter = np.argsort(w)
+    w = w[sorter]
+    v = v[:,sorter]
+
+    for T in Ts:
+        print('part2: T=%f' % T)
+        Z = np.sum(np.exp(-w/T))
+        s = 1/Z * sum(multi_dot((v[:,i].T, S, v[:,i])) * np.exp(-w[i]/T) for i in range(len(w)))
+        Ss.append(s)
+    Ss = np.array(Ss)
+
+    plt.plot(Ts, Ss)
+    plt.ylabel('S')
+    plt.xlabel('T')
+    plt.show()
+
+
 fermions = 4 # number of fermions
+sites = 4 # number of sites
+flavors = 2*sites # number of flavors
+dim_fock = 2**flavors
 dim = comb(flavors, fermions)
+states = np.arange(dim_fock)
+
+t = 1.0
+U = 1.0
+Us = np.linspace(0.0, 4.0, num=100)
+Ts = np.linspace(0.0, 1.0, num=1000)
 
 print('fock space dimension = %d' % dim_fock)
 print('%d fermions sector dimension = %d' % (fermions, dim))
 
 annihilators = get_annihilators(flavors)
 
-#H = hamilton_ring(4, 1, 0, annihilators)
-#print('H is hermitian = %r' % (H == HT).all())
-#N = get_n(annihilators)
-#Sz = get_sz(annihilators)
-#print('H and N commutes = %r' % (H.dot(N) == N.dot(H)).all())
-#print('H and Sz commutes = %r' % (H.dot(Sz) == Sz.dot(H)).all())
+check_annihilators(annihilators)
+check_H(annihilators)
 
-def part1(annihilators, fermions, t, Umin, Umax, Usteps):
-    # spectrum for n fermions; U=Umin..Umax; t
-    Us = np.linspace(Umin, Umax, num=Usteps)
-    Es = []
-    Ss = []
-    mask = n_sector_mask(dim_fock, fermions)# & sz_sector_mask(dim_fock, 1)
-    print('part1: sector dimension %d' % np.sum(mask))
-    S = get_s(annihilators)
-    S4 = get_sector(S, mask)
-    for U in Us:
-        print('part1: U=%f' % U)
-        H = hamilton_ring(fermions, t, U, annihilators)
-        H4 = get_sector(H, mask)
-        w, v = np.linalg.eigh(H4)
-        w[np.abs(w) < 1e-5] = 0
-        v[np.abs(v) < 1e-5] = 0
-        sorter = np.argsort(w)
-        w = w[sorter]
-        v = v[:,sorter]
-        Es.append(w)
-        s = multi_dot((v[:,0].T, S4, v[:,0]))[0,0]
-        Ss.append(s)
-    Es = np.array(Es)
-    Ss = np.array(Ss)
-    
-    for i in range(min(100, Es.shape[1])):
-        plt.plot(Us, Es[:,i], label='E%d' % i)
-    plt.xlabel('U')
-    plt.ylabel('Ei')
-    plt.legend()
-    plt.show()
-    
-    plt.plot(Us, Ss)
-    plt.xlabel('U')
-    plt.ylabel('S(gs)')
-    plt.show()
-#part1(annihilators, fermions, 1.0, 0.0, 4.0, 50)
+mask = n_sector_mask(dim_fock, fermions)# & sz_sector_mask(dim_fock, 0)
 
-def part2(annihilators, fermions, t, U, Tmin, Tmax, Tsteps):
-    Ts = np.linspace(Tmin, Tmax, num=Tsteps)
-    Ss = []
-    mask = n_sector_mask(dim_fock, fermions)
-    
-    H = hamilton_ring(fermions, t, U, annihilators)
-    H4 = get_sector(H, mask)
-    w, v = np.linalg.eigh(H4)
-    S = get_s(annihilators)
-    S4 = get_sector(S, mask)
-    
-    for T in Ts:
-        Z = np.sum(np.exp(-w/T))
-        s = 1/Z * sum(multi_dot((v[:,i].T, S4, v[:,i]))[0,0] * np.exp(-w[i]/T) for i in range(len(w)))
-        Ss.append(s)
-    Ss = np.array(Ss)
-    
-    plt.plot(Ts, Ss)
-    plt.ylabel('S')
-    plt.xlabel('T')
-    plt.show()
-part2(annihilators, fermions, 1.0, 1.0, 0.01, 10.0, 500)
+print('sector dimension %d' % np.sum(mask))
+
+part1(annihilators, mask, t, Us)
+part2(annihilators, mask, t, U, Ts)
 
--- ue3.py
+++ ue3.py
@@ -4,40 +4,42 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 delta = 0.1
-t = 3
+t = 1
 a = 1
 X = 0.5
 
 KPOINTS = 100
 WPOINTS = 201
-ws = np.linspace(-30, 30, WPOINTS)
+
+ws = np.linspace(-30, 30, num=WPOINTS)
 ks_tmp = np.linspace(0, 2*np.pi/a, num=KPOINTS, endpoint=False)
 ks = np.array(np.meshgrid(ks_tmp, ks_tmp)).reshape(2, -1).T
+epss = [ 0, 1.5, 3, 5, 10, 10 ]
+mus = [ 0, 0, 0, 0, 0, -5 ]
 
-#eps_k = lambda a,t,kx,ky: -2*t*(np.cos(kx*a)+np.cos(ky*a))
-#eps = np.mean([eps_k(a,t,kx,ky) for kx,ky in ks])
 eps_k = lambda a,t,ks: -2*t*(np.cos(ks[:,0]*a)+np.cos(ks[:,1]*a))
 
-eps = 10
-mu = -eps/2
-print('mu', mu)
+for eps, mu in zip(epss, mus):
+    print('mu', mu)
 
-siw = np.zeros(WPOINTS)
-for i in range(10):
-    print(i)
-    siw_old = siw
-    
-    f = lambda w, s: np.mean(1/(w+mu-eps_k(a,t,ks)-s+1j*delta))
-    giw = np.vectorize(f)(ws, siw)
-    Giw = 1/(1/giw + siw)
-    giw = (1-X)*Giw + X*(1/(1/Giw-eps))
-    siw = 1/Giw - 1/giw
-    
-    Aiw = -1/np.pi * np.imag(giw)
-    
-    if (np.sum(np.abs(siw-siw_old)) < 1e-6):
-        break
+    siw = np.zeros(WPOINTS)
+    for i in range(10):
+        print(i)
+        siw_old = siw
+        
+        f = lambda w, s: np.mean(1/(w+mu-eps_k(a,t,ks)-s+1j*delta))
+        giw = np.vectorize(f)(ws, siw)
+        Giw = 1/(1/giw + siw)
+        giw = (1-X)*Giw + X*(1/(1/Giw-eps))
+        siw = 1/Giw - 1/giw
+        
+        Aiw = -1/np.pi * np.imag(giw)
+        
+        if (np.sum(np.abs(siw-siw_old)) < 1e-6):
+            break
 
-plt.plot(ws, Aiw)
+    plt.plot(ws, Aiw, label='mu %f eps %f' % (mu, eps) )
+
+plt.legend()
 plt.show()
 
